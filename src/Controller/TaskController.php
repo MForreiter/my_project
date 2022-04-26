3cc350596c8ef54f7075d9ddcfcd20647d7d65c7
MForreiter <104354097+MForreiter@users.noreply.github.com>
2022-04-26 11:05:33 +0200

    Formv2

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\Type\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/task/new", name="task_new")
+
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($task);
+            $entityManager->flush();
+            return $this->redirectToRoute('task_success', ['id' => $task->getId() ]);
+        }
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * @Route("/task/success/{id}", name="task_success")
+
+     */
+    public function success($id)
+    {
+        return $this->render('task/success.html.twig', ['id' => $id]);
+
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $task;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dueDate;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Task
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    /**
+     * @param mixed $task
+     * @return Task
+     */
+    public function setTask($task)
+    {
+        $this->task = $task;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * @param mixed $dueDate
+     * @return Task
+     */
+    public function setDueDate($dueDate)
+    {
+        $this->dueDate = $dueDate;
+        return $this;
+    }
+
+
+}
